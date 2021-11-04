e5c1c031968d296bc1dd95f72f37cf24d187007c
Micherenio Ferreira <mistermiherenio@gmail.com>
2021-11-04 19:39:28 -0300

    API cripto moeda

---
+++ main.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.lang import Builder
+import requests
+
+GUI = Builder.load_file("tela.kv")
+
+class MeuAplicativo(App):
+    def build(self):
+        return GUI
+    
+    def on_start(self):
+
+        self.root.ids["moeda1"].text = f"Bitcoin R${self.pegar_cotacao('BTC')}"
+        self.root.ids["moeda2"].text = f"Ethereun R${self.pegar_cotacao('ETH')}"
+
+    def pegar_cotacao(self, moeda):
+        link = f"https://economia.awesomeapi.com.br/last/{moeda}-BRL"
+        requisicao = requests.get(link)
+        dic_requisicao = requisicao.json()
+        cotacao = dic_requisicao[f"{moeda}BRL"]["bid"]
+        return cotacao
+
+MeuAplicativo().run()
